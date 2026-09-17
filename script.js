ea5c5a8ba8e31086010dc8aae40c3ad78b2a89c0
AaronTooNice <256012394+ChriistopherAaron@users.noreply.github.com>
2026-09-16 20:59:50 -0700

    Add dynamic grid cells and update HTML with slider controls

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const GRIDSIDE = 600;
-let rows = 16;
-let cols = 16;
-
+let squaresPerSides = 16;
 
 const sketchArea = document.querySelector("#sketch-area");
-sketchArea.style.width = `${GRIDSIDE}px`;
-sketchArea.style.height = `${GRIDSIDE}px`;
+const sliderContainer = document.querySelector("#slider-container");
+const slider = document.querySelector("#slider");
+const sliderValue = document.querySelector("#slider-value");
 
+sliderValue.textContent = `${slider.value} x ${slider.value} (Resolution)`;
+sketchArea.style.width = sketchArea.style.height = `${GRIDSIDE}px`;
 
-function createGridCells() {
-    for (let i = 0; i < (rows * cols); i++) {
+function setBackgroundColor () {
+    this.style.backgroundColor = "black";
+}
+
+function createGridCells(squaresPerSide) {
+    const numOfSquares = (squaresPerSides * squaresPerSides);
+    const widthOrHeight = `${(GRIDSIDE / squaresPerSides) -2}px`;
+    for (let i = 0; i < numOfSquares; i++) {
         const gridCell = document.createElement("div");
 
-        gridCell.style.width = `${(GRIDSIDE / cols) -2}px`;
-        gridCell.style.height = `${(GRIDSIDE / rows) -2}px`;
+        gridCell.style.width = gridCell.style.height = widthOrHeight;
         gridCell.classList.add("cell");
 
         sketchArea.appendChild(gridCell);
+
+        gridCell.addEventListener("mouseover", setBackgroundColor);
     }
 }
 
 
-createGridCells();
+
+
+
+createGridCells(16);
