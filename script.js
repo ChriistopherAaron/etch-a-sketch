84224fefb7b8688e519dff6908679a50c66011e1
AaronTooNice <256012394+ChriistopherAaron@users.noreply.github.com>
2026-09-16 20:37:27 -0700

    Update script.js logic

--- script.js
+++ script.js
@@ -9,10 +9,16 @@ sketchArea.style.height = `${GRIDSIDE}px`;
 
 
 function createGridCells() {
-    for (let i = 0; i < (row * cols); i++) {
+    for (let i = 0; i < (rows * cols); i++) {
         const gridCell = document.createElement("div");
 
         gridCell.style.width = `${(GRIDSIDE / cols) -2}px`;
-        gridCell.sytle.height = `${(GRIDSIDE / rows) -2}px`;
+        gridCell.style.height = `${(GRIDSIDE / rows) -2}px`;
+        gridCell.classList.add("cell");
+
+        sketchArea.appendChild(gridCell);
     }
 }
+
+
+createGridCells();
